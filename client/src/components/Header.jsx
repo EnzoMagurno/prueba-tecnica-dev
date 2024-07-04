ab88092ab73e00aed820a7d44bf8110acab425cb
EnzoMagurno <magurnoenzo31602@gmail.com>
2024-07-04 18:18:26 -0300

    styles into Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import logo from '../../public/ecommerce.webp'
+import { FaShoppingCart } from "react-icons/fa";
+
 
 const Header = () => {
+
+  const handleCart = () =>{
+    
+  }
+
   return (
     <div className='logo-div'>
-      <img  src={logo} alt="E-commerce Logo" />
+    <div className='logo-name'>
+      <img src={logo} alt="E-commerce Logo" onClick={()=>window.location.reload()}/>
       <h2>Products</h2>
-      
     </div>
+    <div className='cart-div'>
+      <FaShoppingCart className='cart' onClick={handleCart}/>
+    </div>
+  </div>
 )
 }
 
